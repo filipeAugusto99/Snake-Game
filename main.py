726fbc7e0c0264e12c0efe00bd2b5bbfe5ad4cdb
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-01-26 19:37:19 -0300

    Detect Collisions with food and Create a Scoreboard and Keep Score

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from turtle import Screen
+
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -20,6 +23,8 @@ for _ in range(0, 3):
 """
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -29,9 +34,14 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
 
+    # detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.increase_score()
 screen.exitonclick()
---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+import turtle
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 24, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.goto(0, 260)
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.update_score()
+
+
+    def update_score(self):
+        self.clear()
+        self.write(arg=f"Score = {self.score}", move=False, align=ALIGNMENT, font=FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.update_score()
